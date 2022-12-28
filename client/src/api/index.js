f42ee445ae951d9e19fcea5264078afbed287700
OkenHaha <keithellakpamoken@gmail.com>
2022-12-28 15:22:49 +0530

    Hello

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "stack-overflow-clone-production.up.railway.app"})
+const API = axios.create({ baseURL: "https://stack-overflow-clone-production.up.railway.app"})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){

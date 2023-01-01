481d117a2e1db0e429bd4a1bd7535b6997364ccd
OkenHaha <keithellakpamoken@gmail.com>
2023-01-01 20:38:47 +0530

    Added phone number input field

--- client/src/components/Chatbot/Chatbot.jsx
+++ client/src/components/Chatbot/Chatbot.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
-import ChatBot from 'react-chatbot-kit';
+import React, {useState} from 'react'
+import './Chatbot.css'
+import ChatBot from 'react-chatbot-kit'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
-import './style.css';
-import config from './config';
-import MessageParser from './MessageParser';
-import ActionProvider from './ActionProvider';
+import ActionProvider from './ActionProvider'
+import MessageParser from './MessageParser'
+import config from './config'
 
-export default function App() {
 
+const Chatbot = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState()
+  const [isVerified, setIsVerified] = useState(false);
   const toggleChatbot = () => {
     setIsOpen(!isOpen);
   };
-  const [isVerified, setIsVerified] = useState(false)
-  const tri = () => {
-    setIsVerified(!isVerified)
-  }
-
   const one = true
+  const tri = () => {
+    setIsVerified(!isVerified);
+  };
+
+  const saveMessages = (messages, HTMLString) => {
+    localStorage.setItem('chat_messages', JSON.stringify(messages));
+  };
+
+  const loadMessages = () => {
+    const messages = JSON.parse(localStorage.getItem('chat_messages'));
+    return messages;
+  };
 
   return (
     <div>
@@ -26,13 +37,23 @@ export default function App() {
       </button>
       {isOpen && (
         <div className="chatbox">
-          {one && (
-        <button onClick={tri} className="button2">Click me</button>
-        )}
-          {isVerified && (
+        {one && (
+            <button onClick={tri} className="button2">
+              Click me
+            </button>
+          )}
+
+        <PhoneInput
+      placeholder="Enter phone number"
+      value={value}
+      onChange={setValue}/>
+
+        {isVerified && (
           <ChatBot
             className="bot"
             config={config}
+            messageHistory={loadMessages()}
+            saveMessages={saveMessages}
             messageParser={MessageParser}
             actionProvider={ActionProvider}
           />
@@ -42,3 +63,5 @@ export default function App() {
     </div>
   );
 }
+
+export default Chatbot

93843a2c14bba5c50e31ace7152ba5fce35a4bb9
OkenHaha <keithellakpamoken@gmail.com>
2023-01-11 22:22:43 +0530

    trying to finish the task

--- client/src/Pages/Community/MainPage.jsx
+++ client/src/Pages/Community/MainPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './MainPage.css'
+
+import LeftBar from './LeftBar'
+import Post from './Post'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const MainPage = () => {
 	return (
-		<div>
-			
+		<div className="home-container-3">
+			<LeftBar/>
+			<div className="home-container-4">
+				<Post/>
+				<RightSidebar/>
+			</div>
 		</div>
 	)
 }
